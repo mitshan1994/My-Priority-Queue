e3b13029b314c9d123828cd53ac1cebc50dd2980
Mit Shan <mitshan1994@gmail.com>
2017-04-08 21:12:08 +0800

    Almost finish the priority queue definition

--- priority_queue.h
+++ priority_queue.h
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <cstdlib>  // exit()
 #include <vector>
 #include <cstddef>  // size_t
 #include <memory>  // std::move()
@@ -24,7 +26,8 @@ public:
         { }
 
     // member functions
-    void Insert(const T &val); // insert a literal?
+    void Insert(const T &val);
+    // Need user to test is empty before calls
     T PopMax();
     bool IsEmpty() const;
     std::size_t Size() const;
@@ -34,12 +37,15 @@ public:
     PriorityQ<T> operator=(const PriorityQ<T> &pq);
     PriorityQ<T> operator=(PriorityQ<T> &&pq);
 
-//private:
+    // helper function
+    void Print();
+
+private:
     size_t size_;
     std::vector<T> heap_;
 
     // help functions used internally
-    void PercolateUp(int hole, T val);
+    void PercolateUp(int hole,const T &val);
     void PercolateDown(int hole);
     void BuildHeap();
 };
@@ -81,7 +87,7 @@ void PriorityQ<T>::PercolateDown(int hole)
 }
 
 template <typename T>
-void PriorityQ<T>::PercolateUp(int hole, T val)
+void PriorityQ<T>::PercolateUp(int hole, const T &val)
 {
     int parent;
     while (hole / 2 > 0) {
@@ -99,4 +105,85 @@ void PriorityQ<T>::PercolateUp(int hole, T val)
 template <typename T>
 void PriorityQ<T>::Insert(const T &val)
 {
+    if (size_ == heap_.size() - 1)
+        heap_.resize(2 * (size_ + 1));
+    ++size_;
+    PercolateUp(size_, val);
+}
+
+template <typename T>
+PriorityQ<T>::PriorityQ(const std::vector<T> &v)
+    : size_(v.size()), heap_(2 * size_ + 1)
+{
+    for (auto it = v.cbegin(); it != v.cend(); ++it)
+        heap_[it - v.cbegin() + 1] = *it;
+    BuildHeap();
+}
+
+template <typename T>
+template <typename Iterator>
+PriorityQ<T>::PriorityQ(Iterator first, Iterator last)
+    : size_(last - first), heap_(2 * size_ + 1)
+{
+    for (auto it = first; it != last; ++it)
+        heap_[it - first + 1] = *it;
+    BuildHeap();
+}
+
+template <typename T>
+T PriorityQ<T>::PopMax()
+{
+    if (size_ == 0) {
+        std::cerr << "Pop to a zero-size priority queue!!!\n";
+        exit(EXIT_FAILURE);
+    }
+    T tmp = heap_[1];
+    heap_[1] = heap_[size_];
+    --size_;
+    PercolateDown(1);
+    return tmp;
+}
+
+template <typename T>
+bool PriorityQ<T>::IsEmpty() const
+{
+    return size_ == 0;
+}
+
+template <typename T>
+std::size_t PriorityQ<T>::Size() const
+{
+    return size_;
+}
+
+template <typename T>
+void PriorityQ<T>::Clear()
+{
+    size_ = 0;
+}
+
+template <typename T>
+PriorityQ<T> PriorityQ<T>::operator=(const PriorityQ<T> &pq)
+{
+    size_ = pq.size_;
+    heap_ = pq.heap_;
+}
+
+template <typename T>
+PriorityQ<T> PriorityQ<T>::operator=(PriorityQ<T> &&pq)
+{
+    size_ = pq.size_;
+    heap_ = std::move(heap_);
+}
+
+template <typename T>
+void PriorityQ<T>::Print()
+{
+    if (size_ == 0) {
+        std::cout << "Empty priority queue.\n";
+        return;
+    }
+    for (int i = 1; i <= size_; ++i)
+        std::cout << heap_[i] << " ";
+    std::cout << std::endl;
 }
--- use_pq.cc
+++ use_pq.cc
@@ -1,14 +1,43 @@
 #include <iostream>
+#include <vector>
 #include "priority_queue.h"
 
 using namespace std;
 
+// need to change all fields to public to use this test function
+void original_test();
+
 int main()
 {
-    PriorityQ<int> pq{10, 3, 2, 100, 50, 60, 55};
-    for (int i = 1; i <= pq.size_; ++i)
-        cout << pq.heap_[i] << "  ";
-    cout << endl;
-
+    PriorityQ<int> pq{1, 2, 3, 5, 4};
+    pq.Print();
+    
     return 0;
+}
+
+void original_test()
+{
+    // PriorityQ<int> pq{10, 3, 2, 100, 50, 60, 55};
+    // for (int i = 1; i <= pq.size_; ++i)
+    //     cout << pq.heap_[i] << "  ";
+    // cout << endl;
+    // pq.Insert(1000);
+    // pq.Insert(39);
+    // for (int i = 1; i <= pq.size_; ++i)
+    //     cout << pq.heap_[i] << "  ";
+
+    // cout << "\n Stage 2:\n";
+    // vector<double> v = {1.5, 3, 4, 2, 5, 0, -3, 6};
+    // PriorityQ<double> pq2(v);
+    // for (int i = 1; i <= pq2.size_; ++i)
+    //     cout << pq2.heap_[i] << "  ";
+    // cout << endl;
+
+    // PriorityQ<double> pq3(v.begin(), v.end());
+    // for (int i = 1; i <= pq3.size_; ++i)
+    //     cout << pq3.heap_[i] << "  ";
+    // cout << endl;
+    // cout << "pq3.PopMax() = " << pq3.PopMax() << endl;
+    // for (int i = 1; i <= pq3.size_; ++i)
+    //     cout << pq3.heap_[i] << "  ";
 }
